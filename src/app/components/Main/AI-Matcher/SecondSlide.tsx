81ecbb02ba40018604a9c0dd1f1989d16ff96fb7
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-08-24 08:44:24 +0700

    added tailwind-scrollbar-hide module to hide scrollbar, added base for endless scrolling for subject/passion picker for second slide of ai matcher

--- src/app/components/Main/AI-Matcher/SecondSlide.tsx
+++ src/app/components/Main/AI-Matcher/SecondSlide.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 
 const SecondSlide = () => {
+  const [scrollEnabled, setScrollEnabled] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setScrollEnabled(true);
+    }, 3000);
+
+    return () => clearTimeout(timer)
+  }, []);
+
   return (
-    <div className="w-full h-full">
-      <div className="relative w-full h-full">
-        <div className="flex flex-col gap-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+    <div className={`w-full h-screen ${scrollEnabled ? "overflow-y-scroll" : "overflow-hidden"} scrollbar-hide`}>
+      <div className="relative w-full h-[200vh] justify-center items-center scroll-smooth">
+        <div className="absolute top-[50vh] left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 gap-4">
           <motion.div
             className="text-lg font-medium"
             initial={{ y: 0, opacity: 1 }}
@@ -19,7 +29,7 @@ const SecondSlide = () => {
           </motion.div>
         </div>
         <motion.div 
-          className="flex h-screen grid grid-cols-3 grid-rows-5 gap-2"
+          className="h-full w-full grid grid-cols-3 grid-rows-5 gap-2"
           initial={{ y: 500, opacity: 0 }}
           animate={{ y: 0, opacity: 1, transition: {
             delay: 3,
@@ -35,7 +45,7 @@ const SecondSlide = () => {
           <div className="bg-gray-500 rounded-xl flex justify-center items-center row-span-2 col-span-1 text-black smooth-animation">6</div>
           <div className="bg-gray-500 rounded-xl flex justify-center items-center row-span-2 col-span-1 text-black smooth-animation">7</div>
           <div className="bg-gray-500 rounded-xl flex justify-center items-center row-span-1 col-span-1 text-black smooth-animation">8</div>
-          <div className="bg-gray-500 rounded-xl flex justify-center items-center row-span-2 col-span-1 text-black smooth-animation">9</div>
+          <div className="bg-gray-500 rounded-xl flex justify-center items-center row-span-1 col-span-1 text-black smooth-animation">9</div>
         </motion.div>
       </div>
     </div>

ffb5cf676f2ea65f87b7ae8afcb0f42671e75414
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-08-31 07:45:11 +0700

    updated checkmark, confirm dialog and selection counter

--- src/app/components/Main/AI-Matcher/SecondSlide/Checkmark.tsx
+++ src/app/components/Main/AI-Matcher/SecondSlide/Checkmark.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { FaCheck } from 'react-icons/fa';
+import React from "react";
+import { motion } from "framer-motion";
+import { FaCheck } from "react-icons/fa";
 
 const Checkmark = ({ isSelected }) => (
-  <motion.div 
-    className={`absolute bottom-2 right-2 p-2 bg-black rounded-full ${isSelected ? 'opacity-100' : 'opacity-0'}`}
+  <motion.div
+    className={`absolute bottom-3 right-3 p-3 bg-black rounded-full ${isSelected ? "opacity-100" : "opacity-0"}`}
     initial={{ opacity: 0 }}
     animate={{ opacity: isSelected ? 1 : 0 }}
-    transition={{ duration: 0.2 }}
+    transition={{ duration: 0.5 }}
   >
-    <FaCheck size={12} />
+    <FaCheck size={16} />
   </motion.div>
 );
 
--- src/app/components/Main/AI-Matcher/SecondSlide/SelectionCounter.tsx
+++ src/app/components/Main/AI-Matcher/SecondSlide/SelectionCounter.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { motion } from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
 const SelectionCounter = ({ count, onConfirm }) => (
-  <motion.div 
-    className="absolute top-6 right-0 bg-black border border-gray-500 p-3 gap-x-2 z-[1000] rounded-lg shadow-md flex items-center"
+  <motion.div
+    className="absolute top-6 right-0 bg-[#003dcc] px-5 text-white py-3 gap-x-2 z-[1000] rounded-lg shadow-md flex items-center"
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     transition={{ duration: 0.5 }}
   >
-    <span className="mr-2">{count}/10 </span>
-    <button 
-      className="bg-white hover:bg-[#f4b034] text-black font-bold py-2 px-4 rounded-full transition-colors ease-in-out duration-500"
+    <span className="mr-2 font-medium">{count}/10 </span>
+    <button
+      className="bg-[#003dcc] border-[#003dcc] border-2 hover:border-[#e1960c] font-bold py-2 px-4 rounded-full transition-colors ease-in-out duration-500"
       onClick={onConfirm}
     >
       Confirm

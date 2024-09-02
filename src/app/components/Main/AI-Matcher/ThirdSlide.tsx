6f6a786e124f428f65540c789682f889330d4680
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-02 09:26:16 +0700

    updated ThirdSlide layout - TODO: make it so that when you click on any one of the options it pulls up a lot of photos from different universities that show that particlular type of setting.

--- src/app/components/Main/AI-Matcher/ThirdSlide.tsx
+++ src/app/components/Main/AI-Matcher/ThirdSlide.tsx
@@ -3,11 +3,12 @@ import React from "react";
 const ThirdSlide = () => {
   return (
     <div className="w-full flex justify-center items-center">
-      <div className="w-3/4 flex flex-col gap-y-10">
-        <div className="text-center text-xl font-medium">
+      <div className="w-full flex flex-col gap-y-10">
+        <div className="text-center text-xl">
           When you think about your ideal college location, do you picture
-          yourself in a bustling city, a quieter suburban area, or a rural
-          settings surrounded by nature?
+          yourself in a <strong>bustling city</strong>, a{" "}
+          <strong>quiet suburban area</strong>, or a{" "}
+          <strong>rural setting</strong> surrounded by nature?
         </div>
         <div className="grid grid-cols-3 grid-rows-1 gap-4 h-[400px]">
           <div className="bg-[#003dcc] rounded-xl items-center justify-center flex scale-100 hover:scale-[105%] transition-all duration-500 ease-in-out">

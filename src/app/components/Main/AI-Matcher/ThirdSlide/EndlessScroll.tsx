269b99638fb6fd85bc58a237b1e085a2d20faa86
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-02 14:01:05 +0700

    fixed hidden overflow x of main content. added x button to close endless scrolling component

--- src/app/components/Main/AI-Matcher/ThirdSlide/EndlessScroll.tsx
+++ src/app/components/Main/AI-Matcher/ThirdSlide/EndlessScroll.tsx
@@ -1,16 +1,18 @@
 import { universityEnvironmentData } from "@/data";
 import UniversityCard from "./UniversityCard";
+import { FaTimes } from "react-icons/fa";
 
 interface EndlessScrollProps {
   selectedCategory: string;
+  onClose: () => void;
 }
 
-const EndlessScroll = ({ selectedCategory }: EndlessScrollProps) => {
+const EndlessScroll = ({ selectedCategory, onClose }: EndlessScrollProps) => {
   const universities = universityEnvironmentData[selectedCategory] || [];
 
   return (
-    <div className="absolute top-0 left-0 w-full h-full z-10 overflow-y-auto bg-black/20 backdrop-blur-sm scrollbar-hide">
-      <div className="flex flex-col gap-4 p-4">
+    <div className="absolute top-0 left-0 w-full h-full z-10 bg-black/20 backdrop-blur-sm">
+      <div className="overflow-y-auto scrollbar-hide flex flex-col gap-8 p-4 h-full">
         {universities.length ? (
           universities.map((university, index) => (
             <UniversityCard key={index} university={university} />
@@ -19,6 +21,12 @@ const EndlessScroll = ({ selectedCategory }: EndlessScrollProps) => {
           <p>No universities available</p>
         )}
       </div>
+      <button
+        onClick={onClose}
+        className="fixed z-50 bottom-6 right-6 bg-white text-black hover:bg-black hover:text-white rounded-full p-5 transition-colors duration-300 ease-in-out"
+      >
+        <FaTimes size={24} />
+      </button>
     </div>
   );
 };
--- src/app/components/Main/AI-Matcher/ThirdSlide/UniversityCard.tsx
+++ src/app/components/Main/AI-Matcher/ThirdSlide/UniversityCard.tsx
@@ -4,13 +4,16 @@ import { motion } from "framer-motion";
 const UniversityCard = ({ university, isActive }) => {
   return (
     <motion.div
-      className={`p-4 bg-black border border-white rounded-lg shadow-md transition-transform duration-300 ${
+      className={`px-20 py-24 bg-[#024534] rounded-lg shadow-md transition-transform h-[500px] duration-300 ${
         isActive ? "scale-105" : ""
       }`}
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3 }}
     >
+      <div className="w-full mb-5 h-3/4 border border-white rounded-2xl p-5 flex justify-center items-center">
+        Image
+      </div>
       <h3 className="text-xl font-semibold">{university.name}</h3>
       <p className="text-gray-300">{university.description}</p>
     </motion.div>

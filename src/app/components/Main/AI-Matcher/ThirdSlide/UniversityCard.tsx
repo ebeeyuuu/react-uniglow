164a0b90610f1847460c02d96a20e1a8617d4cf7
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-02 14:25:01 +0700

    updated university card size and endless scrolling

--- src/app/components/Main/AI-Matcher/ThirdSlide/UniversityCard.tsx
+++ src/app/components/Main/AI-Matcher/ThirdSlide/UniversityCard.tsx
@@ -4,17 +4,19 @@ import { motion } from "framer-motion";
 const UniversityCard = ({ university, isActive }) => {
   return (
     <motion.div
-      className={`px-20 py-24 bg-[#024534] rounded-lg shadow-md transition-transform h-[500px] duration-300 ${
-        isActive ? "scale-105" : ""
+      className={`px-20 py-16 bg-[#024534] rounded-2xl shadow-md transition-transform min-h-[600px] duration-300 flex justify-center items-center flex-col ${
+        isActive ? "scale-105" : "100"
       }`}
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3 }}
     >
-      <div className="w-full mb-5 h-3/4 border border-white rounded-2xl p-5 flex justify-center items-center">
+      <div className="w-full h-2/3 mb-7 border border-white rounded-2xl p-5 flex justify-center items-center">
         Image
       </div>
-      <h3 className="text-xl font-semibold">{university.name}</h3>
+      <h3 className="text-xl font-semibold flex mr-auto mb-2">
+        {university.name}
+      </h3>
       <p className="text-gray-300">{university.description}</p>
     </motion.div>
   );

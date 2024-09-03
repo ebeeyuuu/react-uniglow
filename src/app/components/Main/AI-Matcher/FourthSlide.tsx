5756480cb7ac0928822c3cd766b591f7423bb63b
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-03 11:49:13 +0700

    added progress slider to template out the fourth slide

--- src/app/components/Main/AI-Matcher/FourthSlide.tsx
+++ src/app/components/Main/AI-Matcher/FourthSlide.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import ProgressSlider from "./FourthSlide/ProgressSlider";
 
-const FourthSlide = () => {
+type SlideProps = {
+  onNextSlide: () => void;
+};
+
+const FourthSlide: React.FC<SlideProps> = ({ onNextSlide }) => {
+  const [isValueChanged, setIsValueChanged] = useState(false);
+
+  const handleValueChange = (newValue: number) => {
+    setIsValueChanged(true);
+  };
+
   return (
-    <div>
-      How important is access to cultural attractions like museums, theatres, or
-      music venus in your ideal college town?
+    <div className="w-5/6 h-full flex flex-col justify-center items-center">
+      <p className="text-center">
+        How important is access to cultural attractions like museums, theatres,
+        or music venus in your ideal college town?
+      </p>
+      <div>
+        <ProgressSlider onValueChange={handleValueChange} />
+      </div>
+      {isValueChanged && (
+        <button
+          onClick={onNextSlide}
+          className="mt-5 px-5 py-2 bg-[#003dcc]/70 text-white rounded-xl hover:bg-[#003dcc] transition duration-300 ease-in-out"
+        >
+          Confirm
+        </button>
+      )}
     </div>
   );
 };

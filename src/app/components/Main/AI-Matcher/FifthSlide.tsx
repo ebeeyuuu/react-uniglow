ece473ba4b11bfe9ba68d510618ba3ce72c65877
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-04 20:58:59 +0700

    updated FifthSlide and EndlessScroll.tsx

--- src/app/components/Main/AI-Matcher/FifthSlide.tsx
+++ src/app/components/Main/AI-Matcher/FifthSlide.tsx
@@ -7,7 +7,7 @@ import ExampleGroups from "./FifthSlide/ExampleGroups";
 const FifthSlide = () => {
   return (
     <div className="w-full h-full flex justify-center items-center flex-col gap-20">
-      <p className="text-3xl w-4/5 font-medium">
+      <p className="text-3xl w-4/5 font-medium text-center">
         What kind of clubs or groups would you love to join in college?
       </p>
       <div className="flex flex-col gap-5">

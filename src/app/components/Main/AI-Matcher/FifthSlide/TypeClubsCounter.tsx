4414c687e51dc7c8652868a31cc814c958475097
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-06 11:34:36 +0700

    updated index.ts type clubs, TypeClubs.tsx, TypeClubsCounter.tsx, and FifthSlide.tsx

--- src/app/components/Main/AI-Matcher/FifthSlide/TypeClubsCounter.tsx
+++ src/app/components/Main/AI-Matcher/FifthSlide/TypeClubsCounter.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-const TypeClubsCounter = () => {
-  return <div>TypeClubsCounter</div>;
+interface TypeClubsCounterProps {
+  count: number;
+  totalCount: number;
+  onConfirm: () => void;
+}
+
+const TypeClubsCounter: React.FC<TypeClubsCounterProps> = ({
+  count,
+  totalCount,
+  onConfirm,
+}) => {
+  return (
+    <motion.div
+      className="flex justify-center items-center px-5 py-3 gap-x-2 z-[1000] rounded-xl shadow-xl bg-black/50 backdrop-blur-sm"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <span className="mr-2 font-medium">
+        {count}/{totalCount}
+      </span>
+      <button className="bg-[#003dcc] hover:bg-transparent border border-[#003dcc] hover:border-white font-bold py-2 px-4 rounded-full transition-colors ease-in-out duration-500">
+        Confirm
+      </button>
+    </motion.div>
+  );
 };
 
 export default TypeClubsCounter;

7bbaa505453efbc2091bf3a2994adfef6665999a
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-06 09:26:59 +0700

    attempted to add placeholder div that changes col span upon resizing but didn't work :()

--- src/app/components/Main/AI-Matcher/FifthSlide/Checkmark.tsx
+++ src/app/components/Main/AI-Matcher/FifthSlide/Checkmark.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { FaCheck } from "react-icons/fa";
 
-const Checkmark = () => {
-  return <div>Checkmark</div>;
-};
+const Checkmark = ({ isSelected }) => (
+  <motion.div
+    className={`absolute bottom-3 right-3 p-3 bg-black rounded-full ${isSelected ? "opacity-100" : "opacity-0"}`}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: isSelected ? 1 : 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <FaCheck size={16} />
+  </motion.div>
+);
 
 export default Checkmark;

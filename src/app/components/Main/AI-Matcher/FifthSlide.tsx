eaa961465946923cf40820ef3031f8132d656208
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-07 14:36:57 +0700

    fixed animation order for fifth slide and added example clubs

--- src/app/components/Main/AI-Matcher/FifthSlide.tsx
+++ src/app/components/Main/AI-Matcher/FifthSlide.tsx
@@ -1,10 +1,8 @@
 import { motion } from "framer-motion";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import TypeClubs from "./FifthSlide/TypeClubs";
-import TypeGroups from "./FifthSlide/TypeGroups";
 import ExampleClubs from "./FifthSlide/ExampleClubs";
-import ExampleGroups from "./FifthSlide/ExampleGroups";
 
 import { clubTypes } from "@/data";
 
@@ -14,22 +12,32 @@ type SlideProps = {
 
 const FifthSlide: React.FC<SlideProps> = ({ onNextSlide }) => {
   const [animateTypeClubs, setAnimateTypeClubs] = useState(false);
+  const [animateIntroText, setAnimateIntroText] = useState(true);
+  const [showExampleClubs, setShowExampleClubs] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setAnimateTypeClubs(true);
+      setAnimateIntroText(false);
     }, 5000);
-  });
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const typeClubsConfirm = useCallback(() => {
+    setAnimateTypeClubs(false);
+    setAnimateIntroText(false);
+    setShowExampleClubs(true);
+  }, []);
 
   return (
     <div className="w-full h-[100vh] flex justify-center items-center overflow-y-auto scrollbar-hide relative">
-      {!animateTypeClubs && (
+      {animateIntroText && (
         <motion.p
           className="text-3xl w-4/5 font-medium text-center"
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
           transition={{ duration: 0.8, ease: "easeInOut", delay: 3 }}
-          onAnimationComplete={() => setAnimateTypeClubs(true)}
         >
           What kind of clubs or groups would you love to join in college?
         </motion.p>
@@ -41,7 +49,18 @@ const FifthSlide: React.FC<SlideProps> = ({ onNextSlide }) => {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.8, ease: "easeInOut", delay: 1 }}
         >
-          <TypeClubs clubTypes={clubTypes} onConfirm={() => {}} />
+          <TypeClubs clubTypes={clubTypes} onConfirm={typeClubsConfirm} />
+        </motion.div>
+      )}
+
+      {showExampleClubs && (
+        <motion.div
+          className="absolute top-0 left-0 w-full h-full"
+          initial={{ x: "100%" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
+          <ExampleClubs />
         </motion.div>
       )}
     </div>

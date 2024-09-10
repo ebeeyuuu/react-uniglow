f19d7f204313af8cadad7bf0512484a0291b1c4e
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-10 12:05:32 +0700

    started working on the mapping function for example clubs with types first

--- src/app/components/Main/AI-Matcher/FifthSlide/ExampleClubs.tsx
+++ src/app/components/Main/AI-Matcher/FifthSlide/ExampleClubs.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { clubExamples } from "@/data";
+
+interface Club {
+  name: string;
+  university: string;
+  club_description: string;
+  university_description: string;
+}
+
+interface ClubExamples {
+  section: Club[];
+}
 
 const ExampleClubs = () => {
-  return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="text-3xl font-semibold">Example Clubs:</div>
-    </div>
-  );
+  return <div className="w-full h-full flex justify-center items-center"></div>;
 };
 
 export default ExampleClubs;

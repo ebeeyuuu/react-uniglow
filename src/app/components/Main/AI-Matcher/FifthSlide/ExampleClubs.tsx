998c19e4e97ce9f20474437df72eb80d9075ea8a
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-10 12:40:02 +0700

    mapped club examples

--- src/app/components/Main/AI-Matcher/FifthSlide/ExampleClubs.tsx
+++ src/app/components/Main/AI-Matcher/FifthSlide/ExampleClubs.tsx
@@ -9,11 +9,38 @@ interface Club {
 }
 
 interface ClubExamples {
-  section: Club[];
+  [section: string]: Club[];
 }
 
 const ExampleClubs = () => {
-  return <div className="w-full h-full flex justify-center items-center"></div>;
+  return (
+    <div className="w-full h-full flex justify-center items-center relative overflow-y-auto overflow-x-hidden scrollbar-hide">
+      <div className="absolute top-0 left-0 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {Object.entries(clubExamples).map(([section, clubs]) => (
+          <div
+            key={section}
+            className="selection min-h-[500px] flex justify-center border rounded-xl p-10 flex-col gap-4 text-wrap"
+          >
+            <div className="text-lg font-bold">{section}</div>
+            <div>
+              {clubs.map((club, index) => (
+                <div className="flex flex-col mb-8 gap-2" key={index}>
+                  <div className="text-lg font-semibold">{club.name}</div>
+                  <div className="text-base font-medium">{club.university}</div>
+                  <div className="text-xs font-light">
+                    {club.club_description}
+                  </div>
+                  <div className="text-xs font-light">
+                    {club.university_description}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ExampleClubs;

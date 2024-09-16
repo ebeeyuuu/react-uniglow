e84116f1917a1c847460be90291c804589992379
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-16 07:53:24 +0700

    updated 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const Page404 = () => {
   return (
     <div className="w-full flex justify-center items-center h-screen">
       <div className="flex flex-col gap-[20px] items-center">
-        <div className="font-light text-9xl text-[#0070e0]">
-          404
-        </div>
-        <div className="text-xl font-bold">
-          Page not found
-        </div>
+        <div className="font-bold text-9xl text-[#0070e0]">404</div>
+        <div className="text-xl font-bold">Page not found</div>
         <div className="">
-          Oops! The page you were looking for doesn't exist. It might have been moved or deleted.
+          Oops! The page you were looking for doesn&apos;t exist. It might have
+          been moved or deleted.
         </div>
-        <div className="px-5 py-3 border border-[#e07000] hover:bg-[#e07000] hover:text-black hover:font-bold smooth-animation">
-          <Link href="/">
-            Back to home
-          </Link>
+        <div className="flex flex-row gap-x-4">
+          <div className="px-5 py-3 border border-[#e07000] hover:bg-[#e07000] smooth-animation">
+            <Link href="/">Back to landing page</Link>
+          </div>
+          <div className="px-5 py-3 border border-[#e07000] hover:bg-[#e07000] smooth-animation">
+            <Link href="/pages/main">Back to home page</Link>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page404
+export default Page404;

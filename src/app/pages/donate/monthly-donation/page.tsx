239a9f04f8fb0e8a0b943e5fd635af6ee8d08a81
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-10-09 20:01:40 +0700

    added placeholder content for donate pages

---
+++ src/app/pages/donate/monthly-donation/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { Suspense, lazy } from "react";
+import Loading from "@/app/components/Loading";
+import NavBar from "@/app/components/NavBar";
+import Footer from "@/app/components/LandingPage/Footer";
+
+const Hero = lazy(() => import("@/app/components/Donate/MonthlyDonation/Hero"));
+
+const Home = () => {
+  return (
+    <Suspense fallback={<Loading />}>
+      <NavBar />
+      <Hero />
+      <Footer />
+    </Suspense>
+  );
+};
+
+export default Home;

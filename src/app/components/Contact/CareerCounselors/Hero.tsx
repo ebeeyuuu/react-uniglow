f806e7ede660d2311c1626f50a48a2100736882e
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-11-07 14:29:11 +0700

    started template files for career-counselors

--- src/app/components/Contact/CareerCounselors/Hero.tsx
+++ src/app/components/Contact/CareerCounselors/Hero.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
-    <div>Hero Career COunselors</div>
-  )
-}
+  return <div>Hero Career Counselors</div>;
+};
 
-export default Hero
+export default Hero;
--- src/app/pages/contact/career-counselors/page.tsx
+++ src/app/pages/contact/career-counselors/page.tsx
@@ -8,12 +8,28 @@ import Footer from "@/app/components/LandingPage/Footer";
 const Hero = lazy(
   () => import("@/app/components/Contact/CareerCounselors/Hero"),
 );
+const Services = lazy(
+  () => import("@/app/components/Contact/CareerCounselors/Services"),
+)
+const Process = lazy(
+  () => import("@/app/components/Contact/CareerCounselors/Process"),
+)
+const Booking = lazy(
+  () => import("@/app/components/Contact/CareerCounselors/Booking")
+)
+const Testimonial = lazy(
+  () => import("@/app/components/Contact/CareerCounselors/Testimonial"),
+)
 
 const Home = () => {
   return (
     <Suspense fallback={<Loading />}>
       <NavBar />
       <Hero />
+      <Services />
+      <Process />
+      <Booking />
+      <Testimonial />
       <Footer />
     </Suspense>
   );
